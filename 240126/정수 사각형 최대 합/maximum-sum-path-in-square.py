9f7ca60f0d3dbbd06e844dfed03fe3ba22a1ec05
Jiyeon Hyun <55076959+jyHyun1014@users.noreply.github.com>
2024-01-26 18:05:23 +0900

    Title: 정수 사각형 최대 합, Time: 169ms, Memory: 73MB, Status: Passed - CodeTree

--- 240126/정수 사각형 최대 합/maximum-sum-path-in-square.py
+++ 240126/정수 사각형 최대 합/maximum-sum-path-in-square.py
@@ -5,24 +5,23 @@ for _ in range(n):
 
 dp = [[0] * n for _ in range(n)]
 
-total = 0
-for i in range(n):
-    total += graph[0][i]
-    dp[0][i] = total
+dp[0][0] = graph[0][0]
+
+for j in range(1, n):
+    dp[0][j] = dp[0][j-1] + graph[0][j]
 
 for i in range(1, n):
-    for j in range(n):
-        if j == 0:
-            dp[i][j] = dp[i-1][j] + graph[i][j]
-        else:
-            dp[i][j] = max(dp[i-1][j] + graph[i][j], dp[i][j-1] + graph[i][j])
+    dp[i][0] = dp[i-1][0] + graph[i][0]
 
+for i in range(1, n):
+    for j in range(1, n):
+        dp[i][j] = max(dp[i-1][j] + graph[i][j], dp[i][j-1] + graph[i][j])
 
 print(dp[n-1][n-1])
 
 # 문제 풀이
-# 1 2 3 4 
-# 5 6 7 8
-# 9 10 11 12
-# 13 14 15 16
+# 1 1 1 1 
+# 1 2 3 4
+# 1 5 6 7
+# 1 8 9 10
 # 순서로 구하면 됨
