bc519c3e2eed995699c7d0a15ddaa57136d2475b
Jiyeon Hyun <55076959+jyHyun1014@users.noreply.github.com>
2024-01-30 00:43:19 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 880ms, Memory: 55MB, Status: Passed - CodeTree

--- 240130/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 240130/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -1,32 +1,30 @@
-from collections import deque
-
 n = int(input())
 graph = [list(map(int, input().split())) for _ in range(n)]
+dp = [[1] * n for _ in range(n)]    # 1로 초기화 하기
 
-dp = [[1] * n for _ in range(n)]
+cells = []
+ans = 0
+
+# 각 칸에 적혀있는 정수값 기준으로 오름차순 정렬
+for i in range(n):
+    for j in range(n):
+        cells.append((graph[i][j], i, j))
+cells.sort()
+
 
 dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
 
-a = []
-
-for i in range(n):
-    for j in range(n):
-        a.append((graph[i][j], i, j))
-
-a.sort(reverse=True)
-
-for v, x, y in a:
-    temp = []
+for v, x, y in cells:
     for k in range(4):
         nx = x + dx[k]
         ny = y + dy[k]
+        # 해당 인덱스의 값이 존재하고, 현재 값 보다 다음 값이 크다면
         if 0 <= nx < n and 0 <= ny < n and v < graph[nx][ny]:
-            temp.append(dp[nx][ny])
-    if temp:
-        dp[x][y] = max(temp) + 1
+            dp[nx][ny] = max(dp[nx][ny], dp[x][y] + 1)
 
-ans = 0
+
+# 전체 수들 중에서 최댓값 찾기
 for i in range(n):
     ans = max(max(dp[i]), ans)
 
