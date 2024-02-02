83f2db926a1da5210ba4dfe59035cd2eb5ec6dd3
Jiyeon Hyun <55076959+jyHyun1014@users.noreply.github.com>
2024-02-03 00:34:45 +0900

    Title: 세 정수형 변수 선언, Time: 101ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240203/세 정수형 변수 선언/declaration-of-three-natural-numbers.py
@@ -0,0 +1,6 @@
+a, b, c = 7, 23, 30
+
+print("%d + %d = %d" % (a, b, c))
+# print("{0} + {1} = {2}".format(a, b, c))
+# print("{new_a} + {new_b} = {new_c}".format(new_a=a, new_b=b, new_c=c))
+# print(f"{a} + {b} = {c}")
