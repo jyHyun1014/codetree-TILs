ff305ce9ce1d7be63953cb12453d826d07c15d6c
Jiyeon Hyun <55076959+jyHyun1014@users.noreply.github.com>
2024-02-03 00:41:55 +0900

    Title: 변수 출력하기 3, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240203/변수 출력하기 3/outputing-variables-3.py
@@ -0,0 +1,6 @@
+a, b, c = 1, 2, 'C'
+
+print("%d->%d->%c" % (a, b, c))
+# print("{0}->{1}->{2}".format(a, b, c))
+# print("{new_a}->{new_b}->{new_c}".format(new_a=a, new_b=b, new_c=c))
+# print(f"{a}->{b}->{c}")
