36d6628742f68c277eb2a35ec34f7e5ca045fa63
Jiyeon Hyun <55076959+jyHyun1014@users.noreply.github.com>
2024-02-04 23:33:57 +0900

    Title: 정수 사각형 최댓값의 최소, Time: 128ms, Memory: 73MB, Status: Passed - CodeTree

--- 240204/정수 사각형 최댓값의 최소/minimax-path-in-square.py
+++ 240204/정수 사각형 최댓값의 최소/minimax-path-in-square.py
@@ -8,7 +8,7 @@ dp[0][0] = graph[0][0]
 for i in range(1, n):
     dp[i][0] = max(dp[i - 1][0], graph[i][0])
 for j in range(1, n):
-    dp[0][j] = max(dp[0][j - 1], graph[0][j - 1])
+    dp[0][j] = max(dp[0][j - 1], graph[0][j])
 
 
 for i in range(1, n):
