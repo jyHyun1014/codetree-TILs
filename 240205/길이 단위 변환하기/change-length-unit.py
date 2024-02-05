ebb8c6b25e5a8b300063f6a47a1df274745f765b
Jiyeon Hyun <55076959+jyHyun1014@users.noreply.github.com>
2024-02-05 21:56:29 +0900

    Title: 길이 단위 변환하기, Time: 143ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240205/길이 단위 변환하기/change-length-unit.py
@@ -0,0 +1,9 @@
+ft_cm = 30.48
+mi_cm = 160934
+
+a = 9.2
+b = 1.3
+
+print("%.1fft = %.1fcm" % (a, a * ft_cm))
+# print("{0}ft = {1:.1f}cm".format(a, a * ft_cm))
+print(f"{b}mi = {b * mi_cm:.1f}cm")
