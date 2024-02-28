3f717846bfaf65eb3a528fe6eeb07eeedb82fd03
Jiyeon Hyun <55076959+jyHyun1014@users.noreply.github.com>
2024-02-28 22:52:57 +0900

    Title: 좀 더 어려운 수학 점수, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240228/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
+++ 240228/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -1,5 +1,5 @@
-a_math, a_eng - map(int, input().split())
-b_math, b_eng - map(int, input().split())
+a_math, a_eng = map(int, input().split())
+b_math, b_eng = map(int, input().split())
 
 if a_math > b_math:
     print('A')
