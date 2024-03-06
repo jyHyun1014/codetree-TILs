fed8dbaa85aad16688162077167f5cd681c02a41
Jiyeon Hyun <55076959+jyHyun1014@users.noreply.github.com>
2024-03-06 23:14:51 +0900

    Title: 홀수만 출력 2, Time: 149ms, Memory: 24MB, Status: Passed - CodeTree

--- 240306/홀수만 출력 2/output-only-odd-2.py
+++ 240306/홀수만 출력 2/output-only-odd-2.py
@@ -1,8 +1,4 @@
 b, a = map(int, input().split())
 
-if b % 2 == 0:
-    for i in range(b-1, a-1, -2):
-        print(i, end=' ')
-else:
-    for i in range(b, a-1, -2):
-        print(i, end=' ')
+for i in range(b, a - 1, -2):
+	print(i, end=" ")
