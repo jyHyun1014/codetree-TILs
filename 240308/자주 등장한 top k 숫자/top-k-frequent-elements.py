6a776f86b502a42994ef0c55b115dec558a6cb41
Jiyeon Hyun <55076959+jyHyun1014@users.noreply.github.com>
2024-03-08 01:49:13 +0900

    Title: 자주 등장한 top k 숫자, Time: 423ms, Memory: 34MB, Status: Passed - CodeTree

--- 240308/자주 등장한 top k 숫자/top-k-frequent-elements.py
+++ 240308/자주 등장한 top k 숫자/top-k-frequent-elements.py
@@ -3,37 +3,17 @@ n , k = map(int, input().split())
 arr = list(map(int, input().split()))
 
 count = dict()
-count2 = dict()
 
+# hashmap에 개수 저장
 for elem in arr:
     if elem in count:
-        count2[count[elem]].remove(elem)
         count[elem] += 1
-        if count[elem] not in count2:
-            count2[count[elem]] = set()
-        count2[count[elem]].add(elem)
-
     else:
         count[elem] = 1
-        if 1 not in count2:
-            count2[1] = set()
-        count2[1].add(elem)
 
-count2_list = list(count2.keys())
-count2_list.sort(reverse=True)
+# [개수, 숫자] 형태를 리스트로 저장
+new_arr = [[value, key] for key, value in count.items()]
+new_arr.sort(reverse=True)
 
-# print(count2_list)
-
-check=False
-temp = 0
-for i in count2_list:
-    ans = list(count2[i])
-    ans.sort(reverse=True)
-    for a in ans:
-        print(a, end=' ')
-        temp += 1
-        if temp == k:
-            check=True
-            break
-    if check:
-        break
+for i in range(k):
+    print(new_arr[i][1], end=' ')
