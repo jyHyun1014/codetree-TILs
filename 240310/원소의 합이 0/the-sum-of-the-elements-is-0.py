2284706d8aa5fe079349da0886dd67711f9c92a0
Jiyeon Hyun <55076959+jyHyun1014@users.noreply.github.com>
2024-03-10 03:47:59 +0900

    Title: 원소의 합이 0, Time: 3287ms, Memory: 25MB, Status: Passed - CodeTree

--- 240310/원소의 합이 0/the-sum-of-the-elements-is-0.py
+++ 240310/원소의 합이 0/the-sum-of-the-elements-is-0.py
@@ -2,12 +2,20 @@ n = int(input())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 c = list(map(int, input().split()))
-d = set(map(int, input().split()))
+d = list(map(int, input().split()))
+
 answer = 0
+dic_d = dict()
+
+for elem in d:
+    if elem in dic_d:
+        dic_d[elem] += 1
+    else:
+        dic_d[elem] = 1
 
 for i in a:
     for j in b:
         for k in c:
-            if -(i + j + k) in d:
-                answer += 1
+            if -(i + j + k) in dic_d:
+                answer += dic_d[-(i + j + k)]
 print(answer)
