d11ad34bfb8a5c420082899ca4f0ab9ffdee1ed7
Jiyeon Hyun <55076959+jyHyun1014@users.noreply.github.com>
2024-03-11 23:03:15 +0900

    Title: 순서를 바꾸었을 때 같은 단어 그룹화하기, Time: 469ms, Memory: 26MB, Status: Passed - CodeTree

--- 240311/순서를 바꾸었을 때 같은 단어 그룹화하기/group-same-word.py
+++ 240311/순서를 바꾸었을 때 같은 단어 그룹화하기/group-same-word.py
@@ -4,9 +4,10 @@ n = int(input())
 dic = defaultdict(int)
 
 for _ in range(n):
-    word = list(input())
-    word.sort()
-    word = "".join(word)
-    dic[word] += 1
+    word = list(input())    # 단어 문자열을 리스트로 변환
+    word.sort()             # 단어 정렬
+    word = "".join(word)    # 리스틑를 문자열로 변환
+    dic[word] += 1          # 단어를 key로, 개수를 value로 저장
 
+# dic의 value 중에 max 값 출력
 print(max(list(dic.values())))
