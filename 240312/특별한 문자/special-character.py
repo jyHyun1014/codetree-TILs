0b7f4d686fc3e820264cc064a3e9fc5a54aed11f
Jiyeon Hyun <55076959+jyHyun1014@users.noreply.github.com>
2024-03-12 23:03:44 +0900

    Title: 특별한 문자, Time: 141ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240312/특별한 문자/special-character.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+
+text = input()
+dic = defaultdict(int)
+answer = ""
+
+# key에 문자를, value에 개수를 저장
+for c in text:
+    dic[c] += 1
+
+# value가 1인 key 찾기
+for c in text:
+    if dic[c] == 1:
+        answer = c
+
+if answer == "":
+    answer = "None"
+
+print(answer)
