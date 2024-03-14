b57be67d06fc7c3a018cd32d2f5d858f1d9c40cc
Jiyeon Hyun <55076959+jyHyun1014@users.noreply.github.com>
2024-03-14 21:24:36 +0900

    Title: a/b 출력, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

--- 240314/a/b 출력/a-divide-b.py
+++ 240314/a/b 출력/a-divide-b.py
@@ -1,10 +1,9 @@
 a, b = map(int, input().split())
 
-number = a//b
-print(number, end="")
-print(".", end="")
+# 정수 부분 먼저 출력
+print(f"{a//b}.", end="")
 
-for i in range(20):
-    a = (a - number * b) * 10
-    number = a // b
-    print(number, end="")
+for _ in range(20):
+    a %= b  # a를 b로 나눈 나머지
+    a *= 10 # 나머지에 10 곱하기
+    print(a // b, end="")
