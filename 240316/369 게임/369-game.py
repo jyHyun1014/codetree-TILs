20fa2e8e6023a69fd6dff92d3c7974185be0f33d
Jiyeon Hyun <55076959+jyHyun1014@users.noreply.github.com>
2024-03-16 23:51:00 +0900

    Title: 369 게임, Time: 146ms, Memory: 24MB, Status: Passed - CodeTree

--- 240316/369 게임/369-game.py
+++ 240316/369 게임/369-game.py
@@ -1,8 +1,8 @@
 n = int(input())
 
 for i in range(1, n + 1):
-    quotient = i // 10
-    remainder = i % 10
+    quotient = i // 10  # 몫
+    remainder = i % 10  # 나머지
     if i % 3 == 0 or quotient in [3, 6, 9] or remainder in [3, 6, 9]:
         print(0, end=' ')
     else:
