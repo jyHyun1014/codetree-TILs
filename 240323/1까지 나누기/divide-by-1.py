ff741c004b7172e632a7342733467c8b5a6e2bb2
Jiyeon Hyun <55076959+jyHyun1014@users.noreply.github.com>
2024-03-23 23:44:55 +0900

    Title: 1까지 나누기, Time: 147ms, Memory: 24MB, Status: Passed - CodeTree

--- 240323/1까지 나누기/divide-by-1.py
+++ 240323/1까지 나누기/divide-by-1.py
@@ -2,9 +2,9 @@ n = int(input())
 count = 0
 div = n
 for i in range(1, n + 1):
-    div /= i
+    div //= i
     count += 1
     if div <= 1:
         print(count)
         break
-    div = int(div)
+    # div = int(div)
