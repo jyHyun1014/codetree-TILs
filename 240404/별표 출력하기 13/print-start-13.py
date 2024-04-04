3d0d39a07587eed0e9e9eb5a588943878ac56aca
Jiyeon Hyun <55076959+jyHyun1014@users.noreply.github.com>
2024-04-04 23:23:57 +0900

    Title: 별표 출력하기 13, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240404/별표 출력하기 13/print-start-13.py
+++ 240404/별표 출력하기 13/print-start-13.py
@@ -1,14 +1,9 @@
 n = int(input())
-cnt_odd = 1
-cnt_even = n
 for i in range(2 * n):
-    if i % 2 == 0:
-        for j in range(cnt_even):
-            print("*", end=" ")
-        print()
-        cnt_even -= 1
-    else:
-        for j in range(cnt_odd):
-            print("*", end=" ")
-        print()
-        cnt_odd += 1
+	if i % 2 == 1:
+		for _ in range(1 + i // 2):
+			print("* ", end="")
+	else:
+		for _ in range(n - i // 2):
+			print("* ", end="")
+	print()
