d0819822be4f8e866c957839ce3b953c55392825
Jiyeon Hyun <55076959+jyHyun1014@users.noreply.github.com>
2024-04-14 23:39:57 +0900

    Title: 알파벳 출력하기, Time: 151ms, Memory: 24MB, Status: Passed - CodeTree

--- 240414/알파벳 출력하기/print-alphabet.py
+++ 240414/알파벳 출력하기/print-alphabet.py
@@ -4,5 +4,9 @@ alphabet = ord('A')
 for i in range(n):
     for j in range(i + 1):
         print(chr(alphabet), end="")
-        alphabet += 1
+        if alphabet == 90:
+            alphabet = ord('A')
+        else:
+            alphabet += 1
+
     print()
