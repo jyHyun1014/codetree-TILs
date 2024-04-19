9f09b6581a8e5c58e9cd4ea7c3a6b3452c8fcc81
Jiyeon Hyun <55076959+jyHyun1014@users.noreply.github.com>
2024-04-19 11:24:26 +0900

    Title: 배열의 특정 위치 찾기 4, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240419/배열의 특정 위치 찾기 4/find-specific-location-of-array-4.py
@@ -0,0 +1,11 @@
+arr = list(map(int, input().split()))
+sum_val = 0
+cnt = 0
+for i in range(len(arr)):
+    if arr[i] == 0:
+        break
+    elif arr[i] % 2 == 0:
+        sum_val += arr[i]
+        cnt += 1
+
+print(cnt, sum_val)
