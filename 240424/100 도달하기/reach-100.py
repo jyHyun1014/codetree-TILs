18dea30a0047056c1c9608033b89e7c28a395556
Jiyeon Hyun <55076959+jyHyun1014@users.noreply.github.com>
2024-04-24 02:08:30 +0900

    Title: 100 도달하기, Time: 176ms, Memory: 24MB, Status: Passed - CodeTree

--- 240424/100 도달하기/reach-100.py
+++ 240424/100 도달하기/reach-100.py
@@ -3,8 +3,7 @@ arr = [1, n]
 
 for i in range(100):
     sum_val = arr[i] + arr[i + 1]
-    print(sum_val, end=" ")
-    if sum_val > 100:
+    arr.append(sum_val)
+    print(arr[i], end=" ")
+    if arr[i] > 100:
         break
-    else:
-        arr.append(sum_val)
