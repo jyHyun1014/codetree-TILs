d9a35f2319bc7138e7b1aeb9ab1a9c08008cf47a
Jiyeon Hyun <55076959+jyHyun1014@users.noreply.github.com>
2024-05-07 01:17:12 +0900

    Title: 대각선으로 숫자 채우기, Time: 174ms, Memory: 25MB, Status: Passed - CodeTree

--- 240507/대각선으로 숫자 채우기/diagonal-numbering.py
+++ 240507/대각선으로 숫자 채우기/diagonal-numbering.py
@@ -7,14 +7,14 @@ arr = [
 num = 1
 for j in range(m):
     k = 0
-    while j - k >= 0:
+    while j - k >= 0 and k < n:
         arr[k][j - k] = num
         k += 1
         num += 1
     
 for i in range(1, n):
     k = 0
-    while i + k < n:
+    while i + k < n and m - 1 - k >= 0:
         arr[i + k][m - 1 - k] = num
         k += 1
         num += 1
