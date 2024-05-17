d61af7880058630e994610326c44df84dcd4d34c
Jiyeon Hyun <55076959+jyHyun1014@users.noreply.github.com>
2024-05-17 22:32:34 +0900

    Title: 문자열 범위 출력하기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240517/문자열 범위 출력하기 2/print-string-in-range-2.py
+++ 240517/문자열 범위 출력하기 2/print-string-in-range-2.py
@@ -5,5 +5,5 @@ if len(s) >= n:
     for i in range(-1, -n - 1, -1):
         print(s[i], end="")
 else:
-    for i in s[-1, -len(s) - 1, -1]:
+    for i in range[-1, -len(s) - 1, -1]:
         print(s[i], end="")
